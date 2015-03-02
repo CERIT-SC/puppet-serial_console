256d98f8c85ca6be0eb5fd8d0e9cad9bbd923e2f
Vlastimil Holer <holer@ics.muni.cz>
2015-03-02 14:22:32 +0100

    Fix unquoted substitution in /etc/default/grub on Debian

--- manifests/kernel/grub2.pp
+++ manifests/kernel/grub2.pp
@@ -9,10 +9,19 @@ class serial_console::kernel::grub2 (
     $value = ["${ttys},${speed}"]
   }
 
+  # Augeas didn't load /etc/default/grub with Shellvars_list.lns on
+  # GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+  exec { 'grub-fix-subst':
+    command => 'sed -i -e \'s/=\(`.*`\)$/="\1"/\' /etc/default/grub',
+    onlyif  => 'grep -F \'=`\' /etc/default/grub',
+    path    => '/bin:/usr/bin',
+  }
+
   kernel_parameter { 'console':
     ensure   => present,
     value    => $value,
     provider => 'grub2',
+    require  => Exec['grub-fix-subst'],
   }
 
   # cleanup
