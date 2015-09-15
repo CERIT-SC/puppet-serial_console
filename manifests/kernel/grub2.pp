78e4a79be89b45767bffb35366b97ed01feb9e0e
Vlastimil Holer <holer@ics.muni.cz>
2015-09-15 14:02:18 +0200

    Remove obsolete cleanups

--- manifests/kernel/grub2.pp
+++ manifests/kernel/grub2.pp
@@ -23,16 +23,4 @@ class serial_console::kernel::grub2 (
     provider => 'grub2',
     require  => Exec['grub-fix-subst'],
   }
-
-  # cleanup
-  augeas { 'grub2_linux_default_cleanup':
-    incl    => '/etc/default/grub',
-    lens    => 'Shellvars.lns',
-    context => '/files/etc/default/grub',
-    notify  => Class['serial_console::refresh'], #???
-    changes => [
-      'rm #comment[. = "Puppet GRUB_CMDLINE_LINUX_DEFAULT console"]',
-      "rm GRUB_CMDLINE_LINUX_DEFAULT[. = '\"\${GRUB_CMDLINE_LINUX_DEFAULT} console=tty0 console=${ttys},115200\"']",
-    ],
-  }
 }
