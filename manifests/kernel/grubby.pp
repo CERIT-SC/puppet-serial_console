7a5522f2680df5e01b22bcac25852c38019e4af6
Vlastimil Holer <holer@ics.muni.cz>
2015-09-15 14:04:48 +0200

    Cleanups

--- manifests/kernel/grubby.pp
+++ manifests/kernel/grubby.pp
@@ -9,22 +9,23 @@ class serial_console::kernel::grubby (
     $kparams = "console=${ttys},${speed}"
   }
 
+  Exec {
+    path => '/bin:/usr/bin:/sbin:/usr/sbin',
+  }
+
   exec { 'grubby-console-clean':
     command => "grubby --update-kernel DEFAULT --remove-args '${kparams}'",
     unless  => "grubby --info DEFAULT | egrep '^args=.*${kparams}'",
-    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
   }
 
   exec { 'grubby-console':
     command => "grubby --update-kernel DEFAULT --args '${kparams}'",
     unless  => "grubby --info DEFAULT | egrep '^args=.*${kparams}'",
-    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
     require => Exec['grubby-console-clean'],
   }
 
   exec { 'grubby-rhgb':
     command => 'grubby --update-kernel DEFAULT --remove-args \'rhgb\'',
     onlyif  => 'grubby --info DEFAULT | egrep \'^args=.*rhgb\'',
-    path    => '/bin:/usr/bin:/sbin:/usr/sbin',
   }
 }
